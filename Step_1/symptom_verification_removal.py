d077db0ae48ffc7db76fa797aa8736799f896ff0
Guilherme Ferreira <guilhermeferreira@seepmode.com>
2023-09-18 21:25:05 +0100

    Added prompt for user, case he chooses so

--- Step_1/symptom_verification_removal.py
+++ Step_1/symptom_verification_removal.py
@@ -33,51 +33,68 @@ def time_limit(seconds):
 
 def is_valid_response(response):
     # Check if the response is 'y', 'n', or follows the format 'y' followed by indices (e.g., 'y01', 'y0123')
-    return bool(re.match(r'^(y(\d{2})*|n)$', response))
+    return bool(re.match(r'^(y(\d{1,2})*|n)$', response))
+
+user_choice = input("Do you want to answer the question yourself (type 'me') or let ChatGPT answer (type 'chatgpt')? ").strip().lower()
 
 def check_symptoms_with_chatgpt(disease, all_symptoms, mismatched_symptoms):
-    content = f"""
-    Based on the information provided, determine if the mismatched symptoms should be removed from the list of all symptoms for the given disease. Respond with 'y' if all mismatched symptoms should be removed. If only some should be removed, respond with 'y' followed by the indices of the symptoms to keep (for example, 'y02' if you wish to keep the first and third symptom). If no symptoms should be removed, respond with 'n'. Ensure your response strictly follows the mentioned format.
-    Disease: {disease}
-    All Symptoms: {all_symptoms}
-    Mismatched Symptoms: {', '.join(mismatched_symptoms)}
-    """
+    # Ask the user if they want to answer themselves or let ChatGPT answer
+
+    # If the user wants to answer themselves
+    if user_choice == 'me':
+        print(f"Disease: {disease}")
+        print(f"All Symptoms: {all_symptoms}")
+        print(f"Length: {len(all_symptoms.split(', '))}")
+        print(f"Mismatched Symptoms: {', '.join(mismatched_symptoms)}")
+        user_input = input("Do you want to remove all mismatched symptoms? (y/n) If you want to keep some symptoms, type 'y' followed by the indices of the symptoms to keep (for example, 'y02' if you wish to keep the first and third symptom). If no symptoms should be removed, respond with 'n': ").strip()
+        while not is_valid_response(user_input):
+            user_input = input("Invalid response. Please answer according to the format. Do you want to remove all mismatched symptoms? (y/n) If you want to keep some symptoms, type 'y' followed by the indices of the symptoms to keep: ").strip()
+        return user_input
+
+    # If the user wants ChatGPT to answer
+    elif user_choice == 'chatgpt':
+        content = f"""
+        Based on the information provided, determine if the mismatched symptoms should be removed from the list of all symptoms for the given disease. Respond with 'y' if all mismatched symptoms should be removed. If only some should be removed, respond with 'y' followed by the indices of the symptoms to keep (for example, 'y02' if you wish to keep the first and third symptom of the mismathed list). If no symptoms should be removed, respond with 'n'. Ensure your response strictly follows the mentioned format.
+        Disease: {disease}
+        All Symptoms: {all_symptoms}
+        Mismatched Symptoms: {', '.join(mismatched_symptoms)}
+        """
 
 
-    responses = []
-    for _ in range(5):
-        with time_limit(10):
-            try:
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        {"role": "system", "content": "You are a sophisticated medical expert assistant. Your primary objective is to meticulously analyze and determine whether the provided symptoms correlate with a specific disease. It's imperative that you adhere strictly to the required output format, ensuring clarity and precision in your response. Dont repeat indicies"},
-                        {"role": "user", "content": content}
-                    ],
-                    max_tokens=10
-                )
-                response_content = response['choices'][0]['message']['content'].strip()
-                if is_valid_response(response_content):
-                    responses.append(response_content)
-            except TimeoutException:
-                pass
+        responses = []
+        for _ in range(5):
+            with time_limit(10):
+                try:
+                    response = openai.ChatCompletion.create(
+                        model="gpt-3.5-turbo",
+                        messages=[
+                            {"role": "system", "content": "You are a sophisticated medical expert assistant. Your primary objective is to meticulously analyze and determine whether the provided symptoms correlate with a specific disease. It's imperative that you adhere strictly to the required output format, ensuring clarity and precision in your response. Dont repeat indicies"},
+                            {"role": "user", "content": content}
+                        ],
+                        max_tokens=10
+                    )
+                    response_content = response['choices'][0]['message']['content'].strip()
+                    if is_valid_response(response_content):
+                        responses.append(response_content)
+                except TimeoutException:
+                    pass
 
-    # Count the frequency of each response
-    response_counts = Counter(responses)
-    most_common_responses = response_counts.most_common()
+        # Count the frequency of each response
+        response_counts = Counter(responses)
+        most_common_responses = response_counts.most_common()
 
-    # Check if we have any valid responses
-    if not most_common_responses:
-        return 'n'
-
-    # If the most common response is 'y' and its frequency is not greater than 5, return the second most common response
-    if most_common_responses[0][0] == 'y' and most_common_responses[0][1] <= 3:
-        if len(most_common_responses) > 1:
-            return most_common_responses[1][0]
-        else:
+        # Check if we have any valid responses
+        if not most_common_responses:
             return 'n'
-    else:
-        return most_common_responses[0][0]
+
+        # If the most common response is 'y' and its frequency is not greater than 5, return the second most common response
+        if most_common_responses[0][0] == 'y' and most_common_responses[0][1] <= 3:
+            if len(most_common_responses) > 1:
+                return most_common_responses[1][0]
+            else:
+                return 'n'
+        else:
+            return most_common_responses[0][0]
 
 # Load the Excel file and the JSON file
 df = pd.read_excel('/home/guilherme/Documents/GitHub/Tese/Dataset_Open_AI/Datasets/step_1/Disease_Dataset_with_new_symptoms.xlsx')
