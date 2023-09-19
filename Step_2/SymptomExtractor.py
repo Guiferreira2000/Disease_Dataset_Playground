2c5f1df9bc53c8dc98fb31833fefffe14475cf9a
Guilherme Ferreira <guilhermeferreira@seepmode.com>
2023-09-19 18:24:23 +0100

    Symptom structure checker

--- Step_2/SymptomExtractor.py
+++ Step_2/SymptomExtractor.py
@@ -5,24 +5,51 @@ class SymptomExtractor:
 
     def __init__(self, input_file):
         self.input_file = input_file
+        self.df = pd.read_excel(self.input_file)
+
+    def preprocess_symptoms(self, output_file):
+        symptom_columns = [col for col in self.df.columns if 'Symptom_' in col]
+    
+        user_decisions = {}  # Record user decisions for each symptom
+
+        for col in symptom_columns:
+            for idx, symptom in enumerate(self.df[col]):
+                if pd.notna(symptom):
+                    # Remove trailing spaces automatically
+                    symptom = symptom.strip()
+
+                    # If the user has already made a decision about this symptom, apply that decision
+                    if symptom in user_decisions:
+                        if user_decisions[symptom]:
+                            self.df[col] = self.df[col].replace(symptom, symptom.capitalize())
+                        continue
+
+                    # If symptom requires capitalization and hasn't been decided on yet, ask the user
+                    capitalized_symptom = symptom.capitalize()
+                    if symptom != capitalized_symptom:
+                        while True:
+                            user_input = input(f"Modify symptom '{symptom}' to '{capitalized_symptom}'? (y/n): ")
+                            if user_input in ['y', 'n']:
+                                break
+                            print("Invalid input. Please enter 'y' or 'n'.")
+
+                        if user_input == 'y':
+                            self.df[col] = self.df[col].replace(symptom, capitalized_symptom)
+                            user_decisions[symptom] = True
+                        else:
+                            user_decisions[symptom] = False
+
+        # Save the preprocessed dataframe to a new Excel file
+        self.df.to_excel(output_file, index=False)
+
+    # ... [The rest of the methods remain unchanged]
 
     def extract_symptoms(self):
-        # Read the dataset
-        df = pd.read_excel(self.input_file)
-
-        # Extract all symptoms into a single list
-        symptom_columns = [col for col in df.columns if 'Symptom_' in col]
-        all_symptoms = df[symptom_columns].values.flatten()
-
-        # Convert to a set to remove duplicates and then back to list
+        symptom_columns = [col for col in self.df.columns if 'Symptom_' in col]
+        all_symptoms = self.df[symptom_columns].values.flatten()
         unique_symptoms = list(set(all_symptoms))
-
-        # Remove None or NaN values
         unique_symptoms = [symptom for symptom in unique_symptoms if symptom is not None and symptom == symptom]
-
-        # Sort the symptoms alphabetically
         unique_symptoms.sort()
-
         return unique_symptoms
 
     def save_to_xlsx(self, symptoms, output_file):
@@ -30,11 +57,9 @@ class SymptomExtractor:
         df.to_excel(output_file, index=False)
 
     def rows_with_gaps(self):
-        df = pd.read_excel(self.input_file)
-        symptom_columns = [col for col in df.columns if 'Symptom_' in col]
-
+        symptom_columns = [col for col in self.df.columns if 'Symptom_' in col]
         gap_info = []
-        for index, row in df.iterrows():
+        for index, row in self.df.iterrows():
             empty_found = False
             for col in symptom_columns:
                 if pd.isna(row[col]):
@@ -42,7 +67,6 @@ class SymptomExtractor:
                 elif empty_found:
                     gap_info.append({"row_index": index, "disease": row["Disease"]})
                     break
-
         return gap_info
 
     def save_to_json(self, gap_info, output_file):
@@ -50,31 +74,21 @@ class SymptomExtractor:
             json.dump({"Rows with gaps": gap_info}, json_file, indent=4)
 
     def get_symptom_frequency(self):
-        df = pd.read_excel(self.input_file)
-        symptom_columns = [col for col in df.columns if 'Symptom_' in col]
-
-        # Flatten all symptom columns into a single series
-        all_symptoms_series = df[symptom_columns].stack()
-
-        # Calculate symptom frequency
+        symptom_columns = [col for col in self.df.columns if 'Symptom_' in col]
+        all_symptoms_series = self.df[symptom_columns].stack()
         frequency = all_symptoms_series.value_counts()
-
         return frequency
 
     def append_frequency_to_xlsx(self, symptoms, output_file):
         df = pd.DataFrame(symptoms, columns=['Symptoms'])
-
-        # Get symptom frequencies and add to the dataframe
         frequencies = self.get_symptom_frequency()
         df['Frequency'] = df['Symptoms'].map(frequencies).fillna(0).astype(int)
-
-        # Switch order of columns
         df = df[['Frequency', 'Symptoms']]
-
         df.to_excel(output_file, index=False)
 
 if __name__ == "__main__":
     extractor = SymptomExtractor("Datasets/step_1/draft.xlsx")
+    extractor.preprocess_symptoms("Datasets/step_1/preprocessed_draft.xlsx")
     symptoms = extractor.extract_symptoms()
     extractor.append_frequency_to_xlsx(symptoms, "Datasets/step_2/output_symptoms_with_frequency.xlsx")
 
